06aa066f819caeba11ae1a32ec4d6d7f4f6bc800
Anatol <trishin.anatoly2018@yandex.ru>
2020-01-11 16:31:15 +0300

    create page of auth and text area for auth

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,11 +1,31 @@
 import React, { Component } from "react";
-
+import classes from "../Auth/Auth.module.css";
+import Button from "../../components/UI/Button/Button";
+import Input from "../../components/UI/Input/Input.js";
 export default class Auth extends Component {
-  render () {
+  registerHandler = () => {};
+  loginHandler = () => {};
+  SubmitHandler = event => {
+    event.preventDefault();
+  };
+  render() {
     return (
-      <div>
-        <h1>Auth</h1>
+      <div className={classes.Auth}>
+        <div>
+          <h1>Авторизация</h1>
+          <form onSubmit={this.SubmitHandler} className={classes.AuthForm}>
+            <Input label="Email" />
+            <Input label="Password" />
+
+            <Button type="success" onClick={this.loginHandler}>
+              Войти
+            </Button>
+            <Button type="primary" onClick={this.registerHandler}>
+              Зарегистрироваться
+            </Button>
+          </form>
+        </div>
       </div>
-    )
+    );
   }
 }
